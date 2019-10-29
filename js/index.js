68a3057a1ea31b137e4b5f41e550a287a28d1e33
msearles25 <skaman4459@gmail.com>
2019-10-29 18:24:50 -0400

    oops i forgot

--- js/index.js
+++ js/index.js
@@ -12,3 +12,62 @@ imgScale.forEach(e => {
         e.style.transition = '0.3s'
     })
 });
+
+// keydown event
+
+const keyDown = document.querySelector('.intro p');
+window.addEventListener('keydown', () => {
+    keyDown.style.background = 'pink';
+});
+
+// wheel
+
+const wheelEvent = document.querySelector('footer p');
+wheelEvent.addEventListener('wheel', () => {
+    wheelEvent.style.fontSize = '30px';
+    wheelEvent.style.color = '#17A2B8';
+});
+
+// drag drop
+
+// load
+window.addEventListener('load', () => {
+    alert('Loaded!');
+});
+
+// focus
+
+const focus = document.querySelector('.content-destination');
+window.addEventListener('focus', () => {
+    focus.style.background = '#FFEBCD';
+});
+
+// resize
+
+const resizeEvent = document.querySelector('.text-content p');
+window.addEventListener('resize', () => {
+    resizeEvent.style.fontWeight = 'bold';
+    resizeEvent.style.fontSize = '20px';
+});
+
+// scroll event
+
+const scrollEvent = document.querySelector('body');
+window.addEventListener('scroll', () => {
+    scrollEvent.style.backgroundColor = '#fc0';
+})
+
+// select 
+
+// const selectEvent = document.querySelector('.logo-heading');
+// selectEvent.addEventListener('select', () => {
+//     document.querySelector('.intro h2').innerHTML = 'You selected some text';
+// });
+
+// double click
+const doubleClick1 = document.querySelectorAll('.destination h4');
+doubleClick1.forEach(e => {
+    e.addEventListener('dblclick', () => {
+        e.style.color = '#17A2B8';
+    })
+})
