45d4bc35725d547fe7a1abe1e67b65f65a25e3c3
msearles25 <skaman4459@gmail.com>
2019-10-29 15:53:29 -0400

    mouse over

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+
+// mouse over to scale bigger middle images
+const imgScale = document.querySelectorAll('.img-content');
+imgScale.forEach(e => {
+    e.addEventListener('mouseover', () => {
+        e.style.transform = 'scale(1.2)';
+        e.style.transition = '0.3s'
+    })
+    e.addEventListener('mouseleave', () => {
+        e.style.transform = 'scale(1)';
+        e.style.transition = '0.3s'
+    })
+});
